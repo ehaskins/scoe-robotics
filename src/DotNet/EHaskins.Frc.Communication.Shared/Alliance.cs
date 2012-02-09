99f2fe24a437620b7570fc67d6956098592b3f8a
Eric Haskins <eric@ehaskins.net>
2012-02-08 21:43:41 -0600

    Removed unused code from solution

--- src/DotNet/EHaskins.Frc.Communication.Shared/Alliance.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace EHaskins.Frc.Communication
-{
-    public enum Alliance : byte
-    {
-        Red = 82,
-        Blue = 66
-    }
-}
--- src/DotNet/Scoe.Beagle.Shared/RobotModel/AnalogInput.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Scoe.Robot.Shared.RobotModel
-{
-    public class AnalogInput : Channel<ushort> { }
-}
