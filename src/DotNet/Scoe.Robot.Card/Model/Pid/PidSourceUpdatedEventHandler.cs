251ec5c56314a04a81330044a3e11c107bc2cddc
Eric Haskins <eric@ehaskins.net>
2012-02-27 23:44:27 -0600

    Cleaning up .net solution and directory.

--- src/DotNet/Scoe.Robot.Card/Model/Pid/PidSourceUpdatedEventHandler.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Scoe.Shared.Model.Pid
-{
-    public delegate void PidSourceUpdatedEventHandler(object sender, PidSourceUpdatedEventArgs e);
-}
