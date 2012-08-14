85e2fae6b44c59a672da2d9cbfa0c525fc672f0e
EHaskins <EHaskins@eric-laptop8.ehnet.ehaskins.net>
2012-08-13 23:48:52 -0500

    Reverting back AIO datasections to single sample versions.

--- src/Arduino/Arduino/ScoeComms/AnalogIODefinition.h
+++ src/Arduino/Arduino/ScoeComms/AnalogIODefinition.h
@@ -8,21 +8,13 @@
 #ifndef ANALOGIODEFINITION_H_
 #define ANALOGIODEFINITION_H_
 
-#include "AnalogIOSample.h"
-
-#define MAX_AIO_SAMPLES 4
 class AnalogIODefinition {
 public:
 	AnalogIODefinition();
 	unsigned char pin;
-	unsigned long lastSampleTime;
-	unsigned int sampleInterval;
-	unsigned int sampleCount;
-	AnalogIOSample samples[MAX_AIO_SAMPLES];
+	unsigned int value;
 	bool enabled;
 	void update();
-	void update(bool force);
-	void commLoop();
 };
 
 #endif /* ANALOGIODEFINITION_H_ */
